32a4a2d665efef76aad70ac8e1bebc2ed58936d3
fernandeeess <matheusfernandesrodrigues@outlook.com>
2024-12-10 21:05:36 -0300

    build: add primeng

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,16 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+
+
 
 import { routes } from './app.routes';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes),importProvidersFrom([BrowserAnimationsModule])
+
+  ]
 };
