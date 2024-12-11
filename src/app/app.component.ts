678ffb1f94bd6bc98316a86b10926be3311ed98e
fernandeeess <matheusfernandesrodrigues@outlook.com>
2024-12-11 01:36:15 -0300

    cria componentes futuros

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { SidebarModule } from 'primeng/sidebar';
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,SidebarModule],
+  imports: [RouterOutlet, HeaderComponent, SidebarModule, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { providePrimeNG } from 'primeng/config';
-
-
 
 import { routes } from './app.routes';
 import { importProvidersFrom } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes),importProvidersFrom([BrowserAnimationsModule])
-
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom([BrowserAnimationsModule])
   ]
 };
