d519de8c2aa86b5cc313d768c6fbbd9119f9bc05
fernandeeess <matheusfernandesrodrigues@outlook.com>
2024-12-21 23:46:55 -0300

    remove info

--- src/app/pages/experiences/experiences.component.ts
+++ src/app/pages/experiences/experiences.component.ts
@@ -20,15 +20,13 @@ export class ExperiencesComponent {
           empresa: 'Stefanini Consultoria e Assessoria em Informática S.A.',
           cargo: 'Analista Desenvolvedor Fullstack',
           periodo: 'Novembro/2023 - Atual',
-          descricao: ["Desenvolvimento de API REST utilizando .NET para integração com sistemas de monitoramento e gestão operacional.","Desenvolvimento de uma plataforma web interativa e responsiva com Angular desenvolvendo componentes reutilizáveis e garantindo uma experiência de usuário otimizada para análise de dados em tempo real.","Desenvolvimento de fluxos de dados com Azure Data Factory e FABRIC K2VIEW."],
-          contrato: 'CLT'
+          descricao: ["Desenvolvimento de API REST utilizando .NET para integração com sistemas de monitoramento e gestão operacional.","Desenvolvimento de uma plataforma web interativa e responsiva com Angular desenvolvendo componentes reutilizáveis e garantindo uma experiência de usuário otimizada para análise de dados em tempo real.","Desenvolvimento de fluxos de dados com Azure Data Factory e FABRIC K2VIEW."]
         },
         {
           empresa: 'Stefanini Consultoria e Assessoria em Informática S.A.',
           cargo: 'Estagiário de Desenvolvimento Backend',
           periodo: 'Fevereiro/2023 - Novembro/2023',
           descricao: ["Desenvolvimento de APIs Rest de estudo utilizando Java e Spring, com integração ao banco de dados PostgreSQL.","Participação em um Grupo de Desenvolvimento de Talentos, onde aprimorei conhecimentos em Java, POO e estruturas de dados." ],
-          contrato: 'Estágio'
         },
         {
           empresa: 'Abraf',
